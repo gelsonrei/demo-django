f971bf887ca4681eb9809ec6dc146fec263e11be
gelsonrei <gelsornei@gmail.com>
2020-03-29 16:17:44 -0300

    ajustado django 1.11

--- common/urls.py
+++ common/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.conf.urls import include, url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('hello.urls'))
+ 
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('hello.urls')),
+
 ]
